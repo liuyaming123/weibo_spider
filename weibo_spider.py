88a4c38627d99432866bd572e3fcb6305db92940
liuyaming <liuyaming@mmbang.net>
2019-02-15 17:39:02 +0800

    微博及其评论爬虫

--- weibo_spider.py
+++ weibo_spider.py
@@ -7,17 +7,23 @@ import requests
 import re
 import time
 import os
+from pymongo import MongoClient
 
 
 '''微博评论爬取'''
 
 
 # 导入配置文件
-config = json.load(open(os.sep.join(['conf','conf.json']),'r'))
+config = json.load(open(os.sep.join(['conf','online_conf.json']), 'r'))
 
 # 日志文件地址
 log_file_path = "{}/logs/".format(config["logs_path"])
 
+# mongo库表
+conn = MongoClient(config["mongodb_conn"]["name"])
+weibo_user = conn.weibo_info.weibo_user
+weibo_topic = conn.weibo_info.weibo_topic
+weibo_comment = conn.weibo_info.weibo_comment  # 创建库表
 
 class Weibo_Spider():
     def __init__(self):
@@ -54,19 +60,37 @@ class Weibo_Spider():
                 if not jsondata.get("ok"):
                     print('\n')
                     break
-                print(url)
-                time.sleep(random.randint(1,2))
+                time.sleep(random.randint(2,4))
                 data = jsondata.get('data').get('data')
                 for d in data:
                     try:
-                        username, text = d.get("user").get("screen_name"), d.get('text')
+                        _id = d.get('id')
+                        weibo_id = id
+                        created_at = d.get('created_at')
+                        source = d.get('source')
+                        user = d.get('user')
+                        text = d.get('text')
+                        reply_id = d.get('reply_id')
+                        reply_text = d.get('reply_text')
+                        like_counts = d.get('like_counts')
+                        liked = d.get('liked')
+
+                        # 入库
+                        weibo_comment.update({'_id': _id}, {
+                            '$set': {'weibo_id': weibo_id, 'comment_api': url, 'created_at': created_at,
+                                     'source': source, 'user': user, 'text': text,
+                                     'reply_id': reply_id, 'reply_text': reply_text, 'like_counts': like_counts,
+                                     'liked': liked, 'crawl_time': int(time.time())}}, upsert=True)
+
+                        # 写日志
+                        username = d.get("user").get("screen_name")
                         if '<' in text:
                             text = self.tags.sub('', d.get("text"))    # 用正则找到html标签并替换为空串
                         cont = 'comment===' + username + ': ' + text
                         self.log(cont,lf_comment)
-                        print(cont)
+
                     except Exception:
-                        self.log(traceback.format_exc(),lf_error)
+                        self.log(traceback.format_exc(), lf_error)
                 print('\n')
             except Exception as e:
                 print(e)
@@ -80,7 +104,6 @@ class Weibo_Spider():
         :return: None
         '''
         index_page = 1
-        cnt = 1    # 统计爬取评论的数量
         while 1:
             url = 'https://m.weibo.cn/api/container/getIndex?containerid='+containerid+'_-_WEIBO_SECOND_PROFILE_WEIBO&page_type=03&page='+str(index_page)
             resp = requests.get(url,headers=self.headers)
@@ -89,26 +112,30 @@ class Weibo_Spider():
             if not resp.get("ok"):
                 print(resp.get("msng"))
                 break
-            time.sleep(random.randint(1,2))
+            time.sleep(random.randint(2,5))
             jsondata = resp["data"]["cards"]
             weibo_cnt = resp["data"]["cardlistInfo"]["total"]
             if index_page == 1:
                 print('当前爬取的用户为---> '+self.name+' <---该用户当前的微博条数为%s条\n\n\n\n'%weibo_cnt)
             for da in jsondata:
-                # print('da:',da)
                 try:
-                    topic_id = da["mblog"]["id"]   # 话题id
-                    topic = da["mblog"]["text"]  # 每一条话题的内容
+                    topic_id = da.get("mblog").get("id")   # 话题id
+                    topic = da.get("mblog").get("text")  # 每一条话题的内容
                     if '<' in topic:
                         topic = self.tags.sub('', topic)
-                    print("正在爬取第%s条微博："%(cnt) + topic)
+                    weibo_topic.update({'weibo_id': topic_id}, {'$set': {'oid': oid, 'topic': topic,'create_time': int(time.time())}}, upsert=True)
+                    print("正在爬取微博：" + topic)
+
+                    # 增量爬
+                    if weibo_comment.find_one({'weibo_id': topic_id}):
+                        print('该微博内容已爬取，正在过滤...\n')
+                        break
+
                     if topic_id:
                         self.detail_p(topic_id)
-                    cnt += 1
-                    time.sleep(random.randint(1,2))
+
                 except Exception:
                     self.log(traceback.format_exc(), lf_error)
-                    # print('da:',da)
             index_page += 1
 
     def get_name(self,oid):
@@ -129,22 +156,28 @@ class Weibo_Spider():
 
 if __name__ == '__main__':
     # 用户唯一标志：oid
-    # oid = '1662214194'  # 北京人不知道的北京事儿
-    # oid = '1742987497'  # 上海地铁
-    # oid = '1655665171'  # 崔玉涛
-    # oid = '1361074815'  # 古天乐
 
-    oids = ['1655665171',]
+    # oid = '1655665171'  # 崔玉涛
+    # oid = '1557303822'  # 儿科医生鱼小南
+    # oid = '1893410897'  # 鲍秀兰诊室
+    # oid = '1086782701'  # 虾米妈咪
+    # oid = '2399301482'  # 医生妈妈欧茜
+    # oid = '2623922575'  # 刘晓雁医生
+
+    oids = ['1655665171','1557303822','1893410897','1086782701','2399301482','2623922575']
 
     for oid in oids:
         containerid = '107603' + oid
+        main_page = 'https://m.weibo.cn/u/'+ oid
 
         # 日志路径
-        lf_comment = "{}/{}.log".format(log_file_path,time.strftime('comment_{}_%Y-%m-%d'.format(containerid), time.localtime()))  # 评论日志路径
+        lf_comment = "{}/{}.log".format(log_file_path,time.strftime('comment_{}_%Y-%m-%d'.format(oid), time.localtime()))  # 评论日志路径
         lf_error = "{}/{}.log".format(log_file_path, time.strftime('error_{}_%Y'.format('107603'), time.localtime()))
 
         # main
         spider = Weibo_Spider()
+        weibo_name = spider.get_name(oid)
+        weibo_user.update({'oid': oid}, {'$set': {'weibo_name': weibo_name, 'main_page': main_page,'create_time': int(time.time())}}, upsert=True)
         spider.get_user_comments(containerid)
 
 
